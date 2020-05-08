4a15d9d92a84ccce0ea0addbf1739b937246621a
Cafeína Design <grupocafeinadesign@gmail.com>
2020-05-08 17:13:06 -0300

    configurações iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSliderModule } from '@angular/material/slider';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 
@@ -15,6 +16,7 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
     MatSliderModule,
     ScullyLibModule
   ],
