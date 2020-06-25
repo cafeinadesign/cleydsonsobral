6537b12a4073e38f1ce8ac84f37fcfbfe1b273f9
Cafeína Design <grupocafeinadesign@gmail.com>
2020-06-25 03:42:32 -0300

    botão de agendar consulta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import pagarme from 'pagarme/browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cleydsonsobral';
+  ngOnInit() {
+  }
+
+  checkout() {
+    console.log('olá!');
+  }
 }
