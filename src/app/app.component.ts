81cb7b9347a288fb4931e5d66450f2d953873cb0
Cafeína Design <grupocafeinadesign@gmail.com>
2020-06-24 22:31:45 -0300

    sobre mim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // title = 'cleydsonsobral';
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  }
 }
