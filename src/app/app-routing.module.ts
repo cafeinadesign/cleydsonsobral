4029fc89ccd87a321204078dcb59241608dd3141
Cafeína Design <grupocafeinadesign@gmail.com>
2020-07-04 06:02:59 -0300

    downgrade angular9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { TermosDeUsoComponent } from './termos-de-uso/termos-de-uso.component';
 import { PoliticaDeCancelamentoComponent } from './politica-de-cancelamento/politica-de-cancelamento.component';
 
+
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'termos-uso', component: TermosDeUsoComponent },
