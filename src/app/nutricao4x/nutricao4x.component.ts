5dcd80eb2e7ff5ac983f6b73d0183689a77e79fe
Cafeína Design <grupocafeinadesign@gmail.com>
2020-07-18 02:07:40 -0300

    prova de conceito

--- src/app/nutricao4x/nutricao4x.component.ts
+++ src/app/nutricao4x/nutricao4x.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import 'bootstrap/js/dist/carousel';
 
 @Component({
   selector: 'app-nutricao4x',
