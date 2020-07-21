32d1eba7a5cd7b81c9acb3a81e60d4ed625d70d9
Cafeína Design <grupocafeinadesign@gmail.com>
2020-07-20 21:06:49 -0300

    new product box

--- src/app/nutricao4x/nutricao4x.component.ts
+++ src/app/nutricao4x/nutricao4x.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import 'bootstrap/js/dist/carousel';
+import bootstrap from 'bootstrap/js/dist/carousel';
 
 @Component({
   selector: 'app-nutricao4x',
