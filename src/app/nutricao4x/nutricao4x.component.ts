2e0c36de109e7d70b6730a87dda326a09673b186
Cafeína Design <grupocafeinadesign@gmail.com>
2020-07-23 22:48:51 -0300

    correçao de bug no carousel

--- src/app/nutricao4x/nutricao4x.component.ts
+++ src/app/nutricao4x/nutricao4x.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import bootstrap from 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/carousel';
 
 @Component({
   selector: 'app-nutricao4x',
   templateUrl: './nutricao4x.component.html',
-  styleUrls: ['./nutricao4x.component.scss']
+  styleUrls: ['./nutricao4x.component.scss'],
 })
 export class Nutricao4xComponent implements OnInit {
-
   constructor(private titleService: Title) {
-    this.titleService.setTitle('Nutrição 4x - O Programa para Emagrecer com a Alimentação Certa');
+    this.titleService.setTitle(
+      'Nutrição 4x - O Programa para Emagrecer com a Alimentação Certa'
+    );
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
