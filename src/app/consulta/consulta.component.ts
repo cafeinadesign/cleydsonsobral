7f9ad2d1e1d8703c66f166180b0978f90b2d1dac
Cafeína Design <grupocafeinadesign@gmail.com>
2020-08-04 00:27:52 -0300

    consulta

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import pagarme from 'pagarme/browser';
 
 @Component({
   selector: 'app-consulta',
@@ -15,12 +16,28 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
   ],
 })
 export class ConsultaComponent implements OnInit {
+  cardForm;
+  carregando = true;
+  card = {
+    card_holder_name: '',
+    card_expiration_date: '',
+    card_number: '',
+    card_cvv: '',
+  };
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor(public auth: AngularFireAuth, private formBuilder: FormBuilder) {}
+  constructor(public auth: AngularFireAuth, private formBuilder: FormBuilder) {
+    this.cardForm = this.formBuilder.group({
+      card_holder_name: '',
+      card_expiration_month: '',
+      card_expiration_year: '',
+      card_cvv: '',
+    });
+  }
 
   ngOnInit(): void {
+    this.carregando = false;
     this.firstFormGroup = this.formBuilder.group({
       cpfCtrl: ['', Validators.required],
       telefoneCtrl: ['', Validators.required],
@@ -31,5 +48,19 @@ export class ConsultaComponent implements OnInit {
     this.secondFormGroup = this.formBuilder.group({
       secondCtrl: ['', Validators.required],
     });
+  }
+  pagar(): void {
+    // pega os erros de validação nos campos do form e a bandeira do cartão
+    const cardValidations = pagarme.validate({ card: this.card });
+    if (cardValidations) {
+      pagarme.client
+        .connect({
+          encryption_key: 'ek_test_OSxEIhfFAQqcqaewfXUr9p9bNpQffG',
+        })
+        .then((client) => client.security.encrypt(this.card))
+        .then((card_hash) => console.log(card_hash));
+    } else {
+      alert('Oops, número de cartão incorreto');
+    }
   }
 }
