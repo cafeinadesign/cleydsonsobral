0acbe109cfb91a77532ed6d3cee537084e0be1cd
Cafeína Design <grupocafeinadesign@gmail.com>
2020-08-08 19:21:27 -0300

    ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,11 +7,11 @@ export const environment = {
     projectId: 'cleydson-sobral',
     storageBucket: 'cleydson-sobral.appspot.com',
     messagingSenderId: '683770682284',
-    appId: '1:683770682284:web:33b4b36cec13d9b308f809',
-    measurementId: 'G-M8P2EH8WWJ',
+    appId: '1:683770682284:web:f1f79869626c67de08f809',
+    measurementId: 'G-XJ00YTCYZF',
   },
   pagarme: {
     encryptionKey: 'ek_live_mOnDtgsLdZMOKkj1bwN3mIk7ycEQP5',
-    url: 'http://localhost:5001/cleydson-sobral/us-central1',
+    url: 'https://us-central1-cleydson-sobral.cloudfunctions.net',
   },
 };
